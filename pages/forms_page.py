68ae6020ede36aee3770d9b27ffb53c749173bb8
MishanyaS <shilinm1203@gmail.com>
2026-07-26 21:23:33 +0300

    FormsPage class

---
+++ pages/forms_page.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages.base_page import BasePage
+
+class FormsPage(BasePage):
+    PRACTICE_FORM_ITEM = (
+        By.XPATH,
+        "//span[text()='Practice Form']",
+    )
+
+    def __init__(self, driver: WebDriver) -> None:
+        super().__init__(driver)
+
+    def open_practice_form(self) -> None:
+        self.click(self.PRACTICE_FORM_ITEM)
+
+    def practice_form_visible(self) -> bool:
+        return self.is_visible(self.PRACTICE_FORM_ITEM)
